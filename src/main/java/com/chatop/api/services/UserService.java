d9f66cf2e487c2b432e8bdac1633c0012817912d
Antoine CANDA <candaantoine@gmail.com>
2024-05-22 12:15:50 +0200

    ✨(exception): add some exception to Authentification and User service

--- src/main/java/com/chatop/api/services/UserService.java
+++ src/main/java/com/chatop/api/services/UserService.java
@@ -1,8 +1,37 @@
 package com.chatop.api.services;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import com.chatop.api.dto.TokenDTO;
+import com.chatop.api.exceptions.NotFoundException;
+import com.chatop.api.models.User;
+import com.chatop.api.repositories.IUserRepository;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ *
+ * @author BarbieSophie
+ */
 @Service
+@Data
+@AllArgsConstructor
 public class UserService {
 
-}
+    private final JwtGeneratorService jwtGeneratorService;
+    private final IUserRepository userRepository;
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserService.class);
+
+    public User getUserFromToken(TokenDTO token){
+        LOGGER.info("Obtention de l'utilisateur a partir du token.");
+
+        String tokenValue = token.getToken();
+        String email = jwtGeneratorService.extractSubject(tokenValue);
+        User user = userRepository.findByEmail(email).orElseThrow(() -> new NotFoundException("User not found"));
+        LOGGER.info("Utilisateur trouve : {}", user.getEmail());
+        
+        return user;
+    }}
